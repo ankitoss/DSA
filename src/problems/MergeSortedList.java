5c3001387a151ac1a4f3ba7c1a23093bb6aeee97
Ankit <ankityadav.live@gmail.com>
2020-05-14 19:53:06 +0530

    solutions for merging 2 sorted lists.

--- src/problems/MergeSortedList.java
+++ src/problems/MergeSortedList.java
@@ -2,6 +2,19 @@ package problems;
 
 import pojo.SinglyLinkedListNode;
 
+/**
+     Merge two sorted linked lists and return it as a new list.
+     The new list should be made by splicing together the nodes of the first two lists, and should also be sorted.
+
+     For example, given following linked lists :
+
+     5 -> 8 -> 20
+     4 -> 11 -> 15
+
+     The merged list should be :
+
+     4 -> 5 -> 8 -> 11 -> 15 -> 20
+ */
 public class MergeSortedList {
 
     public SinglyLinkedListNode mergeTwoLists(SinglyLinkedListNode A, SinglyLinkedListNode B) {
