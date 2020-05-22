04b8f663f04fb7552f741f81062ec9e89100793f
Ankit <ankityadav.live@gmail.com>
2020-05-23 00:17:14 +0530

    updated: problem statement for prev solution.

--- src/problems/MaxSumPathInBinaryTree.java
+++ src/problems/MaxSumPathInBinaryTree.java
@@ -2,6 +2,50 @@ package problems;
 
 import pojo.BinaryTreeNode;
 
+/**
+     Given a binary tree T, find the maximum path sum.
+
+     The path may start and end at any node in the tree.
+
+     Input Format:
+
+     The first and the only argument contains a pointer to the root of T, A.
+
+     Output Format:
+
+     Return an integer representing the maximum sum path.
+
+     Constraints:
+
+     1 <= Number of Nodes <= 7e4
+     -1000 <= Value of Node in T <= 1000
+
+     Example :
+
+     Input 1:
+
+       1
+      / \
+     2   3
+
+     Output 1:
+     6
+
+     Explanation 1:
+     The path with maximum sum is: 2 -> 1 -> 3
+
+     Input 2:
+
+        -10
+        /  \
+     -20  -30
+
+     Output 2:
+     -10
+
+     Explanation 2
+     The path with maximum sum is: -10
+ */
 public class MaxSumPathInBinaryTree {
 
     private static int max = Integer.MIN_VALUE;
